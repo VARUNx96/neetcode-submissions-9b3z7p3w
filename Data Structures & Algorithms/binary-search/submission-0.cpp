fca31a6b28e0cbc170c04370892f3bddd9edd436
VARUN J <70467762+VARUNx96@users.noreply.github.com>
2026-04-05 11:45:17 +0530

    Add: binary-search - submission-0

---
+++ Data Structures & Algorithms/binary-search/submission-0.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int i = 0;
+        while(i < nums.size()){
+            if(nums[i] == target) return i;
+            else i++;
+        }
+        return -1;
+    }
+};
