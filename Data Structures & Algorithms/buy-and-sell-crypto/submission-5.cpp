913f3400e6ce8997945ec78994c2f792d36b0d53
VARUN J <70467762+VARUNx96@users.noreply.github.com>
2026-08-02 18:43:42 +0530

    Add: buy-and-sell-crypto - submission-5

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-5.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int min = prices[0];
+        int profit = 0;
+        for (auto i: prices){
+            if(i < min) min = i;
+            else profit = max(profit,i - min);
+        }
+        return profit;
+    }
+};
